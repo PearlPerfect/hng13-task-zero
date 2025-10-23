929dd620537c3bdeb7258ea2b0f79195a0e8085d
Imo Emmanuel Udoh <perfectpearl2030@gmail.com>
2025-10-24 00:34:14 +0100

    change

--- server.js
+++ server.js
@@ -43,7 +43,8 @@ res.status(500).json({
     });
     }
 });
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
+ app.listen(PORT, "localhost", () => {
+   console.log(`Server running on http://localhost:${PORT}`)
+// app.listen(PORT, '0.0.0.0', () => {
+//   console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
